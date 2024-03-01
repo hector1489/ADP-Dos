c71b574f9d1db15eb66375d9a7c8c7ce202761af
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2024-02-29 22:32:54 -0300

    [fix] svg

--- src/components/Description/Description.tsx
+++ src/components/Description/Description.tsx
@@ -1,41 +1,22 @@
+import './Description.css'
 
 const Description = () => {
-
   return (
-    <div className="d-flex flex-column m-5">
-      <h3>Quienes somos <span className="text-warning">?</span> <span className="text-danger">.</span></h3>
-      <p className="mt-4">
-        La Agrupación <span className="text-warning">"</span><span className="text-success">Amigos del Puangue</span><span className="text-warning">"</span> , una organización con personalidad juridica dedicada
-        al cuidado del medio ambiente, el fomento de la cultura y la música en la comuna de Curacaví,
-        Región Metropolitana de Chile.
+    <div className="custom-description m-5">
+      <h3 className="section-title">¿Quiénes somos?</h3>
+      <p className="section-content">
+        La Agrupación "<span className="highlight">Amigos del Puangue</span>", una organización con personalidad jurídica dedicada al cuidado del medio ambiente, el fomento de la cultura y la música en la comuna de Curacaví, Región Metropolitana de Chile.
 
-        Desde nuestra fundación en elaño 2005, hemos trabajado incansablemente para promover la protección
-        y preservación del entorno natural de la cuenca del río Puangue, así como para impulsar
-        iniciativas culturales y musicales que enriquezcan nuestra comunidad. Nuestra misión es crear
-        conciencia sobre la importancia de conservar nuestro patrimonio natural y cultural, involucrando
-        a la comunidad en actividades que promuevan un desarrollo sostenible y respetuoso con el medio
-        ambiente.
+        Desde nuestra fundación en el año 2005, hemos trabajado incansablemente para promover la protección y preservación del entorno natural de la cuenca del río Puangue, así como para impulsar iniciativas culturales y musicales que enriquezcan nuestra comunidad. Nuestra misión es crear conciencia sobre la importancia de conservar nuestro patrimonio natural y cultural, involucrando a la comunidad en actividades que promuevan un desarrollo sostenible y respetuoso con el medio ambiente.
 
-        Entre nuestras actividades más destacadas se encuentran campañas de limpieza y reforestación en
-        áreas vulnerables, talleres educativos sobre ecología y medio ambiente, eventos culturales como
-        conciertos, exposiciones y ferias artesanales, y programas de voluntariado que involucran a personas
-        de todas las edades en acciones concretas para mejorar nuestro entorno.
+        Entre nuestras actividades más destacadas se encuentran campañas de limpieza y reforestación en áreas vulnerables, talleres educativos sobre ecología y medio ambiente, eventos culturales como conciertos, exposiciones y ferias artesanales, y programas de voluntariado que involucran a personas de todas las edades en acciones concretas para mejorar nuestro entorno.
 
-        Nos enorgullece contar con el apoyo y la colaboración de numerosas instituciones, empresas y
-        voluntarios comprometidos con nuestra causa, así como el reconocimiento de las autoridades locales
-        y regionales por nuestra labor en beneficio de la comunidad y el medio ambiente.
+        Nos enorgullece contar con el apoyo y la colaboración de numerosas instituciones, empresas y voluntarios comprometidos con nuestra causa, así como el reconocimiento de las autoridades locales y regionales por nuestra labor en beneficio de la comunidad y el medio ambiente.
 
-        Estamos siempre abiertos a establecer nuevas alianzas y colaboraciones que nos permitan ampliar el
-        alcance de nuestras actividades y seguir contribuyendo al bienestar de Curacaví y sus habitantes.
-        Por ello, nos gustaría explorar la posibilidad de trabajar juntos en proyectos que beneficien a ambas
-        partes y fortalezcan nuestro compromiso con el desarrollo sostenible y la preservación del patrimonio
-        cultural y natural de nuestra región.
+        Estamos siempre abiertos a establecer nuevas alianzas y colaboraciones que nos permitan ampliar el alcance de nuestras actividades y seguir contribuyendo al bienestar de Curacaví y sus habitantes. Por ello, nos gustaría explorar la posibilidad de trabajar juntos en proyectos que beneficien a ambas partes y fortalezcan nuestro compromiso con el desarrollo sostenible y la preservación del patrimonio cultural y natural de nuestra región.
 
-        Siempre estamos a disposición para cualquier consulta o propuesta de colaboración que deseen plantearnos.
-        Agradecemos de antemano su atención y consideración, y esperamos tener la oportunidad de trabajar
-        juntos en un futuro próximo <span className="text-danger">.</span>
+        Siempre estamos a disposición para cualquier consulta o propuesta de colaboración que deseen plantearnos. Agradecemos de antemano su atención y consideración, y esperamos tener la oportunidad de trabajar juntos en un futuro próximo.
       </p>
-
     </div>
   )
 }
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,36 +1,45 @@
 import logo from '../../assets/img/logo2.jpeg'
 import './Footer.css'
 
-
 const Footer = () => {
-    return (
-        <footer className='footer d-flex justify-content-around align-items-center p-3 mt-3'>
-            <div className="d-flex gap-3 text-white align-items-center">
-                <div className='logo-footer'>
-                <img src={logo} alt="logo"/>
-                </div>
-                <div className='text-white'>
-                    <h5 className='fw-bold'>A.D.P.</h5>
-                    <p>@Amigos Del Puangue.</p>
-                </div>
+  return (
+    <footer className='footer p-3'>
+      <div className="container">
+        <div className="row justify-content-around align-items-center">
+          <div className="col-md-3">
+            <div className='logo-footer'>
+              <img src={logo} alt="logo" />
             </div>
+            <div className='text-white'>
+              <h5 className='fw-bold'>A.D.P.</h5>
+              <p>@Amigos Del Puangue.</p>
+            </div>
+          </div>
+          <div className="col-md-3">
             <div className="text-white">
-                <h5 className='fw-bold'>Patrocinio</h5>
-                <p>@Chichas Duran</p>
-                <p>Vamos! Ayudanos a cuidar.</p>
+              <h5 className='fw-bold'>Patrocinio</h5>
+              <p>@Chichas Duran</p>
+              <p>Vamos! Ayudanos a cuidar.</p>
             </div>
+          </div>
+          <div className="col-md-3">
             <div className="text-white">
-                <h5 className='fw-bold'>Enlaces</h5>
-                <a href="https://www.instagram.com/festin_sin_piedra2024?igsh=NjgxZ292ZTRzY2Nv" target="_blank">Instagram</a>
-                <p>facebook</p>
+              <h5 className='fw-bold'>Enlaces</h5>
+              <a href="https://www.instagram.com/festin_sin_piedra2024?igsh=NjgxZ292ZTRzY2Nv" target="_blank" rel="noopener noreferrer">Instagram</a>
+              <p>Facebook</p>
             </div>
+          </div>
+          <div className="col-md-3">
             <div className="text-white">
-                <h5 className='fw-bold'>Contacto</h5>
-                <p>amigosdelpuangue.cvi@gmail.com </p>
-                <p>Haz click en nustras redes sociales.</p>
+              <h5 className='fw-bold'>Contacto</h5>
+              <p>amigosdelpuangue.cvi@gmail.com </p>
+              <p>Haz click en nuestras redes sociales.</p>
             </div>
-        </footer>
-    );
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
