15fc1c8c67c1278ab4143f64cbc6fd2f8bcfd27a
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2024-05-26 18:31:39 -0400

    [fix] img and text

--- src/components/Fest/Fest.tsx
+++ src/components/Fest/Fest.tsx
@@ -1,18 +1,32 @@
-import festin from '../../assets/img/festin2024.jpeg';
-import './Fest.css';
+import festin from '../../assets/img/festin2024.jpeg'
+import imgEquipo from '../../assets/img/festin2024/equipoFestin2024.png'
+import './Fest.css'
 
 const Fest = () => {
   return (
-    <div className='fest-container d-flex flex-column justify-content-center'>
-      <div className='text-center'>
-        <h3><span className='text-success'>"Festin sin Piedra"</span> 2024 <span className='text-warning'>.</span></h3>
-        <p>Se parte de todas nuestras actividades <span className='text-success fw-bold'>!</span> <span className='text-warning'>.</span></p>
+    <div className='d-flex flex-column align-items-center'>
+      <div className='agradecemos-box d-flex flex-column align-items-center text-center'>
+        <h3>Agradecemos:</h3>
+        <div className="agradecemos-text">
+          <p>
+            Como agrupación, nos enorgullecemos de nuestro equipo de trabajo, quienes,
+            como un puñado de corazones e ideales unidos, dieron vida al Festín Sin Piedra 2024.
+            Damos las gracias a cada uno de ellos y a todas las personas involucradas en esta ardua labor.
+          </p>
+          <p>
+            Por todo el apoyo y compromiso, ¡GRACIAS TOTALES!
+          </p>
+        </div>
+        <img src={imgEquipo} alt="Equipo Festín 2024" className='agradecemos-img' />
       </div>
-      <div className='d-flex justify-content-center p-3'>
-        <img src={festin} alt="festin" className='fest-image' />
+
+      <div className='fest-container d-flex flex-column align-items-center text-center'>
+        <h3><span className='text-success'>"Festín sin Piedra"</span> 2024<span className='text-warning'>.</span></h3>
+        <p>Se parte de todas nuestras actividades <span className='text-success fw-bold'>!</span> <span className='text-warning'>.</span></p>
+        <img src={festin} alt="Festín" className='fest-image' />
       </div>
     </div>
-  );
+  )
 }
 
-export default Fest;
+export default Fest
