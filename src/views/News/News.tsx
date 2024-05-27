284872cbddd9ded26c52f77bb70459561c214b31
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2024-05-27 14:47:14 -0400

    update

--- src/views/News/News.tsx
+++ src/views/News/News.tsx
@@ -4,7 +4,7 @@ import { Fest } from "../../components"
 const News= () => {
 
     return (
-        <div className="d-flex flex-column justify-content-center m-3">
+        <div className="d-flex flex-column justify-content-center m-4">
         <Fest />
         </div>
     )
