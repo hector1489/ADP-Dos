9d44cd4b177210169971482eb623d478858f049f
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2024-05-27 12:20:24 -0400

    [fix] icon contact

--- src/components/Contacto/Contacto.tsx
+++ src/components/Contacto/Contacto.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Form, Button, Alert } from 'react-bootstrap';
+import { Form, Button, Alert, FormGroup } from 'react-bootstrap';
 import emailjs from 'emailjs-com';
 import './contacto.css';
 
@@ -43,13 +43,23 @@ const Contacto = () => {
           <Form.Label>Envíanos un mensaje</Form.Label>
           <Form.Control as="textarea" rows={3} name="message" required />
         </Form.Group>
+        <FormGroup>
+        </FormGroup>
         <Form.Group className='mb-3 d-flex flex-column'>
           <Form.Label>
             <span className='text-info'>Nuestro email </span><span className='text-warning fw-bold'>:</span> amigosdelpuangue.cvi.20@gmail.com
           </Form.Label>
+          <div className="d-flex flex-row justify-content-around">
+          <a className='social-icon' href="https://www.instagram.com/amigosdelpuangue/" target="_blank" rel="noopener noreferrer">
+            <i className="fa-brands fa-instagram fa-2xl"></i>
+          </a>
+          <a className='social-icon' href="https://www.youtube.com/@amigosdelpuangue7242" target="_blank" rel="noopener noreferrer">
+            <i className="fa-brands fa-youtube fa-2xl"></i>
+          </a>
           <Button className="submit-button fw-bold" type="submit" disabled={showSending}>
             {showSending ? 'Enviando...' : 'Enviar'}
           </Button>
+          </div>
         </Form.Group>
         {showError && <Alert variant="danger" className="mt-3">Error al enviar el formulario. Inténtalo de nuevo más tarde.</Alert>}
         {showSuccess && <Alert variant="success" className="mt-3">¡El formulario se envió correctamente!</Alert>}
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -37,10 +37,10 @@ const Footer = () => {
           <div className="d-flex flex-column text-white">
             <h5 className='fw-bold'>Enlaces</h5>
             <div className='d-flex flex-row gap-2'>
-            <a href="https://www.instagram.com/amigosdelpuangue/" target="_blank" rel="noopener noreferrer">
+            <a className='social-icon' href="https://www.instagram.com/amigosdelpuangue/" target="_blank" rel="noopener noreferrer">
               <i className="fa-brands fa-instagram fa-2xl"></i>
             </a>
-            <a href="https://www.youtube.com/@amigosdelpuangue7242" target="_blank" rel="noopener noreferrer">
+            <a className='social-icon' href="https://www.youtube.com/@amigosdelpuangue7242" target="_blank" rel="noopener noreferrer">
               <i className="fa-brands fa-youtube fa-2xl"></i>
             </a>
             </div>
