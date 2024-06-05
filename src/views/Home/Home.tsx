5e0e39799b449eb3c02c8dc59d4965b64c4564e6
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2024-06-05 19:18:52 -0400

    update section

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -5,8 +5,12 @@ const Home = () => {
 
   return (
     <div>
+      <section>
       <IntroGallery />
+      </section>
+      <section>
       <Fest />
+      </section>
       <div className="Home-arrow d-flex mt-2">
         <a href="#top" className="Home-icon m-2">
         <i className="fa-sharp fa-solid fa-angles-up fa-beat-fade fa-2x"></i>
